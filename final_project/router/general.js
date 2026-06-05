04825bb40b3c621cafe447ef07c2a02395e96169
Tom White <twhite4@bates.edu>
2026-06-05 12:58:47 -0400

    Complete task 5 get book reviews route

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -59,10 +59,14 @@ public_users.get('/title/:title', function (req, res) {
 
   return res.status(404).json({ message: "No books found with this title" });
 });
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
 
+  if (book) {
+    return res.status(200).send(JSON.stringify(book.reviews, null, 4));
+  }
+
+  return res.status(404).json({ message: "Book not found" });
+});
 module.exports.general = public_users;
