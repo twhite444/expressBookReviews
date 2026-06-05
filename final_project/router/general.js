ce950fa3b359cc208f31d4201cb9ab07603ab4da
Tom White <twhite4@bates.edu>
2026-06-05 12:58:05 -0400

    Complete task 4 get books by title route

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -43,12 +43,22 @@ public_users.get('/author/:author', function (req, res) {
 
   return res.status(404).json({ message: "No books found by this author" });
 });
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  let matchingBooks = {};
 
+  Object.keys(books).forEach((isbn) => {
+    if (books[isbn].title === title) {
+      matchingBooks[isbn] = books[isbn];
+    }
+  });
+
+  if (Object.keys(matchingBooks).length > 0) {
+    return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
+  }
+
+  return res.status(404).json({ message: "No books found with this title" });
+});
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
